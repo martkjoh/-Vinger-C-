a08fb68432454be5fae0373d509af8ddd69a0323
Martin <martin.johnsrud@gmail.com>
2019-02-09 00:23:13 +0100

    Blackjackklassen implementert. Nå går det unna

--- Øving VSCode/Øving 5/main.cpp
+++ Øving VSCode/Øving 5/main.cpp
@@ -5,74 +5,7 @@ int main()
 {
     seedRandom();
 
-    CardDeck deck{};
-    deck.shuffle();
-    Hand player{deck};
-    player.drawCard(deck);
-    player.drawCard(deck);
-    player.printHand();
-    cout << player.getValue() << endl;
-    cout << player.getTrueBlackjack() << endl;
-    cout << endl;
-    // Blackjack game{};
     
-    // cout << "The dealers first card is " << game.showCardDealer() << endl;
-
-    // // Skal vekk
-    // cout << "The dealers hand: " << game.dealer.showHand() << endl;
-
-    // char answ = 'Y';
-    // while (true)
-    // {   
-    //     cout << "Your cards are " << game.showHandPlayer() << endl;
-    //     cout << "Would you like to draw another card? [Y/N] ";
-    //     cin >> answ;
-    //     answ = toupper(answ);
-
-    //     if (answ == 'Y')
-    //     {
-    //         game.playerDraw();
-    //         if (game.getValuePlayer() > 21) break;
-    //         else continue;
-    //     }
-    //     else if (answ == 'N') break;
-    //     else cout << "Invalid input, try again" << endl;
-    // }
-
-    // cout << "Your cards are " << game.showHandPlayer() << endl;
-
-    // string winner = game.getWinner();
-    // int winningPoints;
-    // int loosingPoints;
-
-    // if (winner == "dealer") 
-    // {
-    //     winningPoints = game.getValueDealer();
-    //     loosingPoints = game.getValuePlayer();
-    // }
-    // else 
-    // {
-    //     winningPoints = game.getValuePlayer();
-    //     loosingPoints = game.getValueDealer();
-    // }
-
-    // if (winner == "draw")
-    // {
-    //     cout << "The game is a draw, the player got" << game.getValuePlayer() << " points,"
-    //     << " and the dealer got " << game.getValueDealer() << " points."; 
-
-    //     if (game.resultOnRealBJ) cout << ", both player and dealer got a real Blackjack." << endl;
-    //     else cout << "." << endl;
-    // }
-
-    // else
-    // {
-    //     cout << "The winner is the " << winner << ", with " << winningPoints << " points";
-    //     if (game.resultOnRealBJ) cout << ", and a real blackjack,";
-    //     else ", ";
-
-    //     cout << " versus " << loosingPoints << " points." << endl;   
-    // }
 
     return 0;
 }
